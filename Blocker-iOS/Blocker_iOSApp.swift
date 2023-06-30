7e156efdd0d0081a10dcd36a561e36e3abed501c
nu-jey <yp50777@naver.com>
2023-06-30 15:51:27 +0900

    Initial Commit

---
+++ Blocker-iOS/Blocker_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blocker_iOSApp.swift
+//  Blocker-iOS
+//
+//  Created by 오예준 on 2023/06/30.
+//
+
+import SwiftUI
+
+@main
+struct Blocker_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
