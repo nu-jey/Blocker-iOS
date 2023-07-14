6e19fed5b0becb2eda4026afbad8607146485ccd
nu-jey <yp50777@naver.com>
2023-07-14 21:42:35 +0900

    - sha256을 통한 해시 값 추출

--- Blocker-iOS/Model/Verification.swift
+++ Blocker-iOS/Model/Verification.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import PDFKit
+import CryptoKit
 class Verification {
     private var contractFile:URL
     init(contractFile: URL) {
@@ -16,17 +17,21 @@ class Verification {
         if let pdf = PDFDocument(url: self.contractFile) {
             let pageCount = pdf.pageCount
             let documentContent = NSMutableAttributedString()
-
+            
             for i in 0 ..< pageCount {
                 guard let page = pdf.page(at: i) else { continue }
                 guard let pageContent = page.attributedString else { continue }
                 documentContent.append(pageContent)
             }
-            print(documentContent.string) // text 데이터
+            let text = documentContent.string
             
             // text 데이터 기반으로 해시값 추출
+            let data = text.data(using: .utf8)
+            let sha256 = SHA256.hash(data: data!)
+            let shaData = sha256.compactMap{String(format: "%02x", $0)}.joined() // 16진법의 결과값으로 변환
             
-            // 추출한 해시 값을 매개변수로 검증 API 호출 
+            // 추출한 해시 값을 매개변수로 검증 API 호출
+            // shaData를 기반으로 검증 API 호출
         }
     }
 }
